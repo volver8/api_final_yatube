76b0242e3f4ce17481b05639c27d67f2ca9d8042
Rusakov Vladimir <rvs1998@yandex.ru>
2024-07-01 01:13:18 +0300

    8 commit

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,6 +1,4 @@
 from rest_framework import permissions
-from rest_framework.exceptions import MethodNotAllowed
-
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     """Класс для проверки авторства."""
